c3e3524a9125ecd3415b9f7e94160d353fdcfd4d
Victor Hugo Chavez <victorchavez997.vc@gmail.com>
2022-01-14 00:53:18 -0600

    Se realiza un ejercicio realizando switch, arrglos, for para obtner la suma, promedio y varianza

--- Javascript-avanzado/arreglos-ciclos/assets/js/ejericico1.js
+++ Javascript-avanzado/arreglos-ciclos/assets/js/ejericico1.js
@@ -9,3 +9,64 @@ for (let i = 0; i < numeros.length; i++) {
 document.write(`El arreglo del ejercicio es ${numeros}`);
 document.write(`<br> La suma de total es de ${suma}<br>`);
 document.write(`<br> El promedio es de ${prom}`);
+
+
+let numerosejercicio2 = [1, 2];
+let sum = 0;
+let rest = 0;
+
+let prome;
+
+for (let n = 0; n < numerosejercicio2.length; n++) {
+    sum += numerosejercicio2[n];
+    rest -= numerosejercicio2[n];
+    /* 15
+     */
+}
+
+document.write(`<br><br>Mi arrego es de ${numerosejercicio2} <br> por lo cual la suma es de ${sum} <br> la resta de los numeros  es de  ${rest}`)
+
+
+
+/******--------------------EJercio extra */
+
+const ejercicio1 = [];
+let pregunta = parseInt(prompt("Ingrese la cantidad de chekes"));
+let suma3 = 0;
+let promen;
+let varianza;
+let cuestionario1;
+let operaciones = prompt("Ingrese que desea calcular (Suma,Promedio o Varianza")
+
+switch (operaciones) {
+    case 'Suma':
+        for (i = 0; i < pregunta; i++) {
+            cuestionario1 = parseInt(prompt("Ingrese los valores que dese sumar"));
+            ejercicio1.push(cuestionario1);
+        }
+        for (i = 0; i < ejercicio1.length; i++) {
+            suma3 += ejercicio1[i];
+        }
+        document.write(`<br><br>Usted tiene ${pregunta} chekes <br> Los valores ingresados son ${ejercicio1} <br>
+         La suma de ellos es de ${suma3}<br>`)
+        break;
+    case 'Promedio':
+        for (i = 0; i < pregunta; i++) {
+            cuestionario1 = parseInt(prompt("Ingrese la calificaciones de los alumnos"));
+            ejercicio1.push(cuestionario1);
+
+        }
+        for (i = 0; i < ejercicio1.length; i++) {
+            suma3 += ejercicio1[i];
+        }
+        document.write(`<br>Usted tiene ${pregunta} alumnos y las calificaciones ingresadas son ${ejercicio1} la suma de ellos es de ${suma3}<br>`)
+        break;
+    case 'Varianza':
+        for (i = 0; i < pregunta; i++) {
+            document.write(`<br>La suma de los valores es ${pregunta}`);
+        }
+        break;
+    default:
+        document.write(`<br><br>No existe la valor indicado`);
+        break;
+}
