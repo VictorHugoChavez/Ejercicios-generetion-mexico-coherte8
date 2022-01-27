46c0913ef9ff9a56dece28cf28e769553200532e
Victor Hugo Chavez <victorchavez997.vc@gmail.com>
2022-01-26 18:15:36 -0600

    Se suben las carpetas de JavaBasiccps,Poryecdto prueba

---
+++ Java/JavaBasicps/src/VariablesTiposDeDatos.java
@@ -0,0 +1,38 @@
+
+public class VariablesTiposDeDatos {
+
+	public static void main (String[] args) {
+		//STRING PERIMTE ALACENAR CADENAS DE TEXTO
+		String nombre ="Victor Hugo";
+		int edad = 24;
+		char inicial= 'C'; /* SE ULTILZA SOLO PARA UN CARACTER, Y SE UTILIZA COMILLAS SIMPLES CON CHAR*/
+		//boolean almacena un valor booleano, puede ser true o false
+		boolean tienesCrush=true;
+		boolean tienesCrushv=false;
+		
+		// tipos de datos enteros 
+
+		//byte
+		byte porcentaje= 18;
+		byte suma=24+98; // si se pass del dato muere la memorio, recuerdos de c++
+		//short
+		short prueba2= 23+127;
+		//int
+		int numero=1021565545;
+		//long
+		long numeroLargo=100_000_000; //es para mayor legibildad en los numeros
+		
+		//float
+		float promedio = 12.4654f;
+		
+		//double
+		double numeroPi= 24564.1345321;
+		
+		System.out.println(numeroLargo);
+		/* https://www.abrirllave.com/java/tipos-de-datos-primitivos.php*/
+		/* https://computandocodigo.wordpress.com/2019/03/03/tipos-de-datos-primitivos-en-java/*/
+		/*Javascript
+		let numero = 10;
+		let nombre ='Victor Hugo'*/
+	}
+}
---
+++ Java/ProyectoPrueba/src/PrimeraClase.java
@@ -0,0 +1,17 @@
+
+public class PrimeraClase {
+	
+	public static void main(String[]args) {
+		System.out.println("Hola Mundo desde Java :-)");
+		System.out.println("FAN DE DENY <3\n");
+		System.out.println("FAN DE REBECA <3\n");
+		System.out.println("FAN DE VIC <3\n");
+		System.out.println("FAN DE OMAR <3\n");
+		System.out.println("FAN DE URIEL <3\n");
+		System.out.println("FAN DE SHAIN <3\n");
+		System.out.println("FAN DE OCTAVIO <3\n");
+		System.out.println("FAN DE IRIS <3\n");
+
+		
+	}
+}
